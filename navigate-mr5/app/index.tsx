61842222647aa45bcb406859f31b0f852a056c7a
Rohit Gayali <vwh2up@virginia.edu>
2025-03-18 16:48:53 -0400

    updated location list
    issue: max call stack exceed on ios

--- navigate-mr5/app/index.tsx
+++ navigate-mr5/app/index.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
-import { Pressable, Text, TextInput, View } from "react-native";
+import { FlatList, Pressable, Text, TextInput, View } from "react-native";
 import { Stack, useLocalSearchParams, useNavigation } from 'expo-router';
 
-function submit() {
-	
-}
+const DATA = ['First Item','Second Item','Third Item',1,2,3,4,5,5,6];
+
+let filteredData = [];
 
 export default function Index() {
   const navigation = useNavigation();
@@ -14,11 +14,33 @@ export default function Index() {
   }, [navigation]);
 	
 	const local = useLocalSearchParams();
-	
-	const [currentLocation, updateCurrentLocation] = useState(local.currentLocation);
+
+	let [showItems, setShowItems] = useState(false);
+		
+	const [currentLocation, updateCurrentLocation] = useState(local.currentLocation || '');
 	const [destination, updateDestination] = useState('');
+
+	function submit() {
+		
+	}
 	
-	console.log(local);
+	function filterData(newLocation) {
+		filteredData = DATA.filter(v => v.toString().toLowerCase().includes(newLocation.toLowerCase()));
+	}
+
+	function currentLocationChanged(newLocation) {
+		filterData(newLocation);
+		updateCurrentLocation(newLocation);
+	}
+	
+	function showList() {
+		filterData(currentLocation);
+		setShowItems(true);
+	}
+	
+	function hideList() {
+		setShowItems(false);
+	}
 	
   return (
     <View
@@ -26,15 +48,26 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+				gap: 20
       }}>
-      <Text style={{ fontSize: 30, margin: 50 }} >Navigate MR5 (BME Labs and Classroom)</Text>
+      <Text style={{ fontSize: 30, marginTop: -200 }} >Navigate MR5 (BME Labs and Classroom)</Text>
+			<View style={{height: 200, zIndex: 2}}>
+				<Text style= {{ padding: 10 }} >Current Location</Text>
+				<TextInput 
+					style={{ borderWidth: 1, padding: 10, margin: 0, width: 300 }}
+					placeholder='Enter current location' placeholderTextColor='#999'
+					onChangeText={ currentLocationChanged } onSubmitEditing={ submit }
+					value={ currentLocation }
+					onFocus={ showList }
+					onBlur={ hideList } />
+					{showItems ? 
+					<FlatList style={{ margin: 0, padding: 0, backgroundColor: 'white', borderWidth: 5, borderColor: 'white',flexGrow: 0, }} contentContainerStyle={{ alignItems: "center", gap: 10 }}
+						data={filteredData}
+						renderItem={({item}) => <Text>{item}</Text>}
+						ListEmptyComponent=<Text>Nothing found</Text> /> : null}
+			</View>
 			<TextInput 
-				style={{ borderWidth: 1, padding: 10, margin: 10, width: 300 }}
-				placeholder='Enter current location' placeholderTextColor='#999'
-				onChangeText={ updateCurrentLocation } onSubmitEditing={ submit }
-				value={ currentLocation } />
-			<TextInput 
-				style={{ borderWidth: 1, padding: 10, margin: 10, width: 300 }}
+				style={{ borderWidth: 1, padding: 10, marginTop: -110, width: 300 }}
 				placeholder='Enter destination' placeholderTextColor='#999'
 				onChangeText={ updateDestination } onSubmitEditing={ submit } />
 				<Pressable style={ ({pressed}) => [{ backgroundColor: 'lightblue', borderRadius: 3, borderColor: pressed ? 'white' : 'lightblue', borderWidth: 1 }] } onPress={ submit } >
