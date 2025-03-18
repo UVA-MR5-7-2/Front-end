67e74c0fb22533ff36e51675af11e4e403c545cb
AnnieA <AnnieArul.05@gmail.com>
2025-03-18 13:17:53 -0400

    Create Main.jsx
    added the main

---
+++ navigate-mr5/app/Main.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import ImageCanvas from './ImageCanvas';
+import GraphComponent from './GraphComponent';
+
+const App = () => {
+  const [nodes, setNodes] = useState([]);
+  const [graph, setGraph] = useState(null);
+
+  return (
+    <div>
+      <h1>Lab Map Navigation</h1>
+      <ImageCanvas setNodes={setNodes} setGraph={setGraph} />
+      {graph && <GraphComponent nodes={nodes} graph={graph} />}
+    </div>
+  );
+};
+
+export default App;
