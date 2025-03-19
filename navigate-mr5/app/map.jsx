ea88db94d07c34e86ca3d6e997bdd7b81971022b
Rohit Gayali <vwh2up@virginia.edu>
2025-03-18 23:30:13 -0400

    Update map.jsx

--- navigate-mr5/app/map.jsx
+++ navigate-mr5/app/map.jsx
@@ -1,7 +1,7 @@
 // import components we need from react and expo
 import { useEffect, useState } from 'react';
 import { Button, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
-import { router, Stack, useLocalSearchParams, useNavigation } from 'expo-router';
+import { router, Stack, useLocalSearchParams, useNavigation, useRootNavigationState } from 'expo-router';
 import { Picker } from '@react-native-picker/picker';
 import ImageCanvas from './Image_Canvas';
 import GraphComponent from './Graph';
@@ -18,15 +18,70 @@ export default function Map() {
 	const location = local.currentLocation;
 	const destination = local.destination;
 	
+  const rootNavigationState = useRootNavigationState();
+	if (rootNavigationState?.key !== undefined) {
+		const locations = ['Select one', 'MR4 Entrance', 'MR6 Entrance', 'Pinn Hall Entrance', 'Lane Road Entrance', 'Atrium', 'Second floor entrance', 'First floor entrance', 'Labs entrance', 'Classroom', 'Restroom', 'BME Faculty Office'];
+		if (!locations.includes(location) || !locations.includes(destination) || location === locations[0] || destination === locations[0] || !router.canGoBack || location === destination) {
+			router.dismissTo("/?currentLocation=")
+		}
+	}
 	
   const [nodes, setNodes] = useState([]);
   const [graph, setGraph] = useState(null);
 
   return (
-    <div>
-      <h1>Lab Map Navigation</h1>
+    <View style={ styles.container }>
+      <Text style={ styles.title }>Navigate MR5: Map</Text>
+			<Text>Current Location: {location}</Text>
+			<Text>Destination: {destination}</Text>
       <ImageCanvas setNodes={setNodes} setGraph={setGraph} />
       {graph && <GraphComponent nodes={nodes} graph={graph} />}
-    </div>
+    </View>
   );
-};
+}
+
+// make it pretty
+const styles = StyleSheet.create({
+  container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 30
+	},
+  title: { 
+		fontSize: 40, 
+		textAlign: 'center' 
+	},
+  PickerContainer: {
+		gap: 2,
+		alignItems	: 'center'
+	},
+  PickerLabel: {
+		padding: 10,
+		fontSize: 20
+	},
+	Picker: {
+		fontSize: 20
+	},
+	button: {
+		backgroundColor: '#2196F3',
+		borderRadius: 3,
+		margin: 20
+	},
+	buttonPressed: {
+		backgroundColor: '#55aef7',
+		borderRadius: 3,
+		margin: 20
+	},
+	buttonText: {
+		fontSize: 20,
+		color: 'white',
+		marginVertical: 10,
+		marginHorizontal: 20,
+	},
+  error: {
+		padding: 10,
+		fontSize: 20,
+		color: 'red'
+	},
+});
