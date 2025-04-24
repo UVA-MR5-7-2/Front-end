c893460b20d8e1a14f428b37311beaf94a089dbf
Rohit Gayali <vwh2up@virginia.edu>
2025-04-24 13:06:35 -0400

    FINAL

--- navigate-mr5/app/map.tsx
+++ navigate-mr5/app/map.tsx
@@ -1,7 +1,7 @@
 // import components we need from react and expo
 import { useEffect, useState } from 'react';
 import { Button, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
-import Svg, { Image, Path, Rect } from 'react-native-svg';
+import Svg, { Image, Path, Rect, Text as SVGText } from 'react-native-svg';
 import { router, Stack, useLocalSearchParams, useNavigation, useRootNavigationState } from 'expo-router';
 import nodes from "./nodes_edges.json"
 const graphlib = require("graphlib");
@@ -71,14 +71,35 @@ export default function Map() {
 				<Text style={ styles.title }>Navigate MR5: Map</Text>
 				<Text style={ styles.text }>Current Location: {location}</Text>
 				<Text style={ styles.text }>Destination: {destination}</Text>
+				<Text style={ styles.description }>The Green square labeled 1 is where you are now. The yellow squares are in-between nodes that you will check off as you head to your destination. As you visit each yellow node, check it off using the checklist below the map. The pink square is your final destination. Follow the green path to navigate to your destination. If you require elevator access, there is an elevator located next to the staircase.</Text>
 			</View>
-			<Svg height="400" width="400" viewBox='0 0 1340 974'>
+			<Svg height="400" width="400" viewBox='0 0 1370 974'>
 				<Image x={0} y={0} width={670} height={974} href={ require('./map 1.png') } />
 				<Image x={670} y={0} width={670} height={974} href={ require('./map 2.png') } />
-				<Path d={'M ' + path.map(v => v.coordinate.join(' ')).join(' ')} stroke='black' strokeWidth='3' fill='none' />
-				<Rect x={ path[0].coordinate[0] - 5 } y={ path[0].coordinate[1] - 5 } width={10} height={10}  fill='black' />
+				
+				<Path d={'M ' + path.map(v => v.coordinate.map((w, i) => i ? w += 8 : w + 3).join(' ')).join(' ')} stroke='black' strokeWidth='7' fill='none' />
+				<Path d={'M ' + path.map(v => v.coordinate.map((w, i) => i ? w += 8 : w + 3).join(' ')).join(' ')} stroke='#00ff00' strokeWidth='4' fill='none' />
+				{
+					path.map(v => 
+						<Rect x={ v.coordinate[0] - 5 + 3 } y={ v.coordinate[1] - 5 + 8 } width={10} height={10}  fill='black' />
+					)
+				}
+				{
+					path.map(v => 
+						<Rect x={ v.coordinate[0] - 3 + 3 } y={ v.coordinate[1] - 3 + 8 } width={6} height={6}  fill='yellow' />
+					)
+				}
+				{
+					path.map((v, i) => 
+						<SVGText x={ v.coordinate[0] + 7 + 3 } y={ v.coordinate[1] - 7 + 8 } fontSize={30} fontFamily="Verdana" fill="navy" stroke='black' strokeWidth={1}>{i+1}</SVGText>
+					)
+				}
+				<Rect x={ path[0].coordinate[0] - 7 + 3 } y={ path[0].coordinate[1] - 7 + 8 } width={14} height={14}  fill='black' />
+				<Rect x={ path[0].coordinate[0] - 5 + 3 } y={ path[0].coordinate[1] - 5 + 8 } width={10} height={10}  fill='#00ff00' />
+				<Rect x={ path[path.length-1].coordinate[0] - 7 + 3 } y={ path[path.length-1].coordinate[1] - 7 + 8 } width={14} height={14}  fill='black' />
+				<Rect x={ path[path.length-1].coordinate[0] - 5 + 3 } y={ path[path.length-1].coordinate[1] - 5 + 8 } width={10} height={10}  fill='#ff00ff' />
 			</Svg>
-			{path.map((v, i) => <label style={ styles.largeText } ><input type="checkbox" id={ 'input-' + i } onInput={ () => { try {document.getElementById('input-' + (i+1)).disabled = false} catch(e){} } } disabled={ !!i } /> {i+1}. <Text style={ styles.largeText } >{v.name}</Text></label>)}
+			{path.map((v, i) => <label style={ styles.label } ><input type="checkbox" id={ 'input-' + i } onInput={ () => { try {document.getElementById('input-' + (i+1)).disabled = false} catch(e){} } } disabled={ !!i } /><Text style={ styles.inputText } >{i+1}. {v.name}</Text></label>)}
     </ScrollView>
   );
 }
@@ -86,19 +107,30 @@ export default function Map() {
 // make it pretty
 const styles = StyleSheet.create({
   container: {
-		flex: 1,
-		alignItems: "center",
 	},
   title: { 
 		fontSize: 40, 
-		margin: 40
+		marginTop: 40,
+		marginBottom: 20,
+		marginLeft: 10
 	},
 	text: {
-		textAlign: 'center', 
-		margin: 10,
-		fontSize: 20
+		fontSize: 20,
+		marginLeft: 10
 	},
-	largeText: {
-		fontSize: 20
+	description: {
+		fontSize: 20,
+		marginTop: 20,
+		marginLeft: 10,
+		marginRight: 10,
+	},
+	label: {
+		fontSize: 20,
+		paddingLeft: 20
+	},
+	inputText: {
+		fontSize: 20,
+		paddingLeft: 10,
+		paddingRight: 10,
 	}
 });
